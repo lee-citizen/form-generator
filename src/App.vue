e7896c2fd75dc8abe086ea7e4360387ad504fbc2
Lee <1792167466@qq.com>
2022-08-18 15:23:52 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <FormIndex></FormIndex>
+  </div>
+</template>
+
+<script>
+import FormIndex from './views/build/index.vue'
+
+export default {
+  name: 'App',
+  components: {
+    FormIndex
+  }
+}
+</script>
+
---
+++ src/plugins/download.js
@@ -0,0 +1,8 @@
+import { saveAs } from 'file-saver'
+
+export default {
+    saveAs(text, name, opts) {
+      saveAs(text, name, opts);
+    },
+}
+
